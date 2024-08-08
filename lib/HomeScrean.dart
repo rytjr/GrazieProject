c04da9d77197b7e42ec942bbffe9c7a43da2a6a6
rytjr <kks007159@naver.com>
2024-08-08 17:19:15 +0900

    Third commit(모달 상세 페이지 구현)

--- lib/HomeScrean.dart
+++ lib/HomeScrean.dart
@@ -21,32 +21,215 @@ class _HomeScreenState extends State<HomeScreen> {
   void _showOrderModal(BuildContext context) {
     showModalBottomSheet(
       context: context,
-      isScrollControlled: true, // 이 설정을 통해 모달창을 전체 화면으로 확장할 수 있습니다.
+      isScrollControlled: true,
       builder: (BuildContext context) {
-        // 화면 크기를 계산합니다.
         final double screenHeight = MediaQuery.of(context).size.height;
         return Container(
-          height: screenHeight - 50, // 상단에서 100만큼의 공간을 남깁니다.
+          height: screenHeight - 60,
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0), // 상단 왼쪽 모서리 둥글게
-              topRight: Radius.circular(20.0), // 상단 오른쪽 모서리 둥글게
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
             ),
           ),
-          child: Center(
-            child: Text(
-              '주문 정보를 입력하세요',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '매장 선택',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    _buildStoreTile(
+                      '구리인창DT',
+                      '경기도 구리시 건원대로 53 (인창동)',
+                      '579m',
+                      'android/assets/images/Grazie2.jpg',
+                    ),
+                    _buildStoreTile(
+                      '구리동대리',
+                      '경기도 구리시 경방로 5, 우진빌딩 (수택동)',
+                      '595m',
+                      'android/assets/images/order.png',
+                    ),
+                    _buildStoreTile(
+                      '다산아이파트',
+                      '경기도 남양주시 도농로 24 (다산동, 북양마을)',
+                      '1.2km',
+                      'android/assets/images/order.png',
+                    ),
+                    _buildStoreTile(
+                      '다산DT',
+                      '경기도 남양주시 다산중앙로 19번길 5 (다산동)',
+                      '1.4km',
+                      'android/assets/images/order.png',
+                    ),
+                    _buildStoreTile(
+                      '도농역',
+                      '경기도 남양주시 다산길 230 (다산동)',
+                      '1.4km',
+                      'android/assets/images/order.png',
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         );
       },
     );
   }
+
+
+  void showStoreDetailsModal(BuildContext context, String title, String address, String imagePath, String operatingHours, String additionalInfo) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        final double screenHeight = MediaQuery.of(context).size.height;
+        return Container(
+          height: screenHeight - 60,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.more_vert),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Image.asset(imagePath, width: MediaQuery.of(context).size.width, height: 200, fit: BoxFit.cover),
+                    SizedBox(height: 20),
+                    Text(title, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                    Text(address, style: TextStyle(fontSize: 18, color: Colors.grey[800])),
+                    SizedBox(height: 10),
+                    Text("운영 시간", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text(operatingHours, style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 20),
+                    Text("추가 정보", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text(additionalInfo, style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("픽업은 어떻게 하시겠어요?", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        pickupButton(context, Icons.local_cafe, "매장 이용"),
+                        pickupButton(context, Icons.shopping_bag, "To-Go"),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget pickupButton(BuildContext context, IconData icon, String text) {
+    return GestureDetector(
+      onTap: () {
+        // 모든 열려 있는 모달을 닫고 Order 페이지로 이동
+        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.pushReplacementNamed(context, '/order'); // '/order'는 Order 페이지로 이동하기 위한 라우트
+      },
+      child: Container(
+        width: 80,
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey, width: 1),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, size: 40, color: Colors.black54),
+            SizedBox(height: 4),
+            Text(text, style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black54)),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildStoreTile(String title, String subtitle, String distance, String imagePath) {
+    return GestureDetector(
+      onTap: () => showStoreDetailsModal(
+          context,
+          title,
+          subtitle,
+          imagePath,
+          "운영 시간: 07:00 - 21:30",
+          "24시간 운영 주말 15시 - 18시, DT로 전환 시간대 조정 가능합니다."
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          height: 110,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.asset(imagePath, width: 90, height: 90, fit: BoxFit.cover),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12.0, top: 8.0, bottom: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                      Text(subtitle, style: TextStyle(fontSize: 12, color: Colors.grey)),
+                      Spacer(),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(distance, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 
   static List<Widget> _widgetOptions = <Widget>[
     HomeContent(),
